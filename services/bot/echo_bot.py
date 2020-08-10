fa8c55faf1139fec837ec6fb922bf226a7cc5f19
iskoroplis <ivanskoroplias@gmail.com>
2020-08-10 12:45:16 +0300

    added pyTelebot

---
+++ services/bot/echo_bot.py
@@ -0,0 +1,10 @@
+import telebot
+
+bot = telebot.TeleBot("1332015866:AAH4KDr10aXlS4qqXrEvo--p4wIC3bQTTuw")
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.reply_to(message, "Parsing-Bot started")
+
+
+bot.polling()
