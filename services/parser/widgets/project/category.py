91df2e15a31ae224562023a9a991f4f22f917e56
noasck <denter425@gmail.com>
2020-11-03 22:46:25 +0200

    added category model

---
+++ services/parser/widgets/project/category.py
@@ -0,0 +1,23 @@
+import re
+from logging import getLogger
+
+logger = getLogger('DOU_JOBS Parser-2.0')
+
+
+class Category:
+    """
+    Class describing category instance.
+    """
+    def __init__(self, link: str, tag: str):
+        """
+        Constructor of category.
+        :param link: link to RSS DOU feed.
+        :param tag: hashtag pf the category consist of alpha characters and nums.
+        """
+        if re.match("[a-zA-Z_0-9]*", tag):
+            self.tag = tag
+            self.link = link
+        else:
+            msg = "Incorrect category tag " + tag
+            logger.warning(msg)
+            raise NameError(msg)
