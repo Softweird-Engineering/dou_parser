ea8fe41033306648b925b9feb8c4867a4b54f041
Moxxiq <dimanavsisto@gmail.com>
2020-11-03 18:48:23 +0200

    moved ADMIN_ID from hardcoded to .env file

--- services/parser/start.py
+++ services/parser/start.py
@@ -1,4 +1,4 @@
-from config import DATABASE_URL, PARSE_URLS, BOT_API, INIT_DB # noqa
+from config import DATABASE_URL, PARSE_URLS, BOT_API, INIT_DB, ADMIN_ID # noqa
 from widgets.project.injectors import DSN # noqa
 from widgets.project.db import create_tables # noqa
 import time
@@ -16,7 +16,7 @@ def start_app():
         create_tables()
 
     print('started')
-    start(PARSE_URLS, BOT_API)
+    start(PARSE_URLS, BOT_API, ADMIN_ID)
 
 
 if __name__ == '__main__':
--- services/parser/widgets/project/echo_bot.py
+++ services/parser/widgets/project/echo_bot.py
@@ -8,74 +8,75 @@ from ..project.controller import is_new_user, process_feed
 
 
 def create_new_user(message):
-	"""
-	Wrapper fot new user coroutine.
-	:param message: message from telebot.
-	:return: void
-	"""
-	asyncio.ensure_future(is_new_user(int(message.chat.id)))
+    """
+    Wrapper fot new user coroutine.
+    :param message: message from telebot.
+    :return: void
+    """
+    asyncio.ensure_future(is_new_user(int(message.chat.id)))
 
 
 async def start_polling(bot_api: str):
-	bot = telebot.AsyncTeleBot(bot_api, threaded=False)
+    bot = telebot.AsyncTeleBot(bot_api, threaded=False)
 
-	@bot.message_handler(commands=['health'])
-	def check_health(message):
-		bot.reply_to(message, "healthy")
+    @bot.message_handler(commands=['health'])
+    def check_health(message):
+        bot.reply_to(message, "healthy")
 
-	@bot.message_handler(commands=['start'])
-	def add_new(message):
-		print("New user!", message.chat.id)
-		create_new_user(message)
-		bot.reply_to(message, "Now you are in our database <3 ")
-	while True:
-		try:
-			await asyncio.sleep(5)
-			updates = bot.get_updates(offset=(bot.last_update_id + 1), timeout=2)
-			bot.process_new_updates(updates)
-		except Exception as e: # noqa
-			print("Something goes wrong!", str(e))
-			await asyncio.sleep(5)
+    @bot.message_handler(commands=['start'])
+    def add_new(message):
+        print("New user!", message.chat.id)
+        create_new_user(message)
+        bot.reply_to(message, "Now you are in our database <3 ")
+    while True:
+        try:
+            await asyncio.sleep(5)
+            updates = bot.get_updates(
+                offset=(bot.last_update_id + 1), timeout=2)
+            bot.process_new_updates(updates)
+        except Exception as e:  # noqa
+            print("Something goes wrong!", str(e))
+            await asyncio.sleep(5)
 
 
 async def check_for_new_jobs(urls: List[str], bot_api: str):
-	tasks = []
-	loop = asyncio.get_event_loop()
-	while True:
-		try:
-			for url in urls:
-				task = loop.create_task(fetch_url(url, bot_api))
-				tasks.append(task)
-			async with timeout(100):
-				await asyncio.gather(*tasks)
-			print("Fetch complete")
-			await asyncio.sleep(7200)
-			print("Be prepared for next fetch...")
-		except asyncio.TimeoutError as t: # noqa
-			print('Timeout exceeded!', str(t))
+    tasks = []
+    loop = asyncio.get_event_loop()
+    while True:
+        try:
+            for url in urls:
+                task = loop.create_task(fetch_url(url, bot_api))
+                tasks.append(task)
+            async with timeout(100):
+                await asyncio.gather(*tasks)
+            print("Fetch complete")
+            await asyncio.sleep(7200)
+            print("Be prepared for next fetch...")
+        except asyncio.TimeoutError as t:  # noqa
+            print('Timeout exceeded!', str(t))
 
 
 async def fetch_url(url: str, bot_api: str):
-	try:
-		bot = telebot.AsyncTeleBot(bot_api, threaded=False)
-		async for resp in process_feed(url):
-			for user_id in resp.users_id:
-				bot.send_photo(user_id, resp.image, resp.message)
-	except Exception as e:  # noqa
-		print("Something goes wrong in posts update!", str(e))
-		await asyncio.sleep(5)
+    try:
+        bot = telebot.AsyncTeleBot(bot_api, threaded=False)
+        async for resp in process_feed(url):
+            for user_id in resp.users_id:
+                bot.send_photo(user_id, resp.image, resp.message)
+    except Exception as e:  # noqa
+        print("Something goes wrong in posts update!", str(e))
+        await asyncio.sleep(5)
 
 
-async def main(urls, bot_api):
-	await is_new_user(775621366)
-	await asyncio.gather(check_for_new_jobs(urls, bot_api), start_polling(bot_api))
+async def main(urls, bot_api, admin_id):
+    await is_new_user(admin_id)
+    await asyncio.gather(check_for_new_jobs(urls, bot_api), start_polling(bot_api))
 
 
-def start(urls: List[str], bot_api: str):
-	"""
-	Main application loop.
-	:param urls: uris to parse.
-	:param bot_api: Telegram Bot API token.
-	:return: void
-	"""
-	asyncio.run(main(urls, bot_api))
+def start(urls: List[str], bot_api: str, admin_id: int):
+    """
+    Main application loop.
+    :param urls: uris to parse.
+    :param bot_api: Telegram Bot API token.
+    :return: void
+    """
+    asyncio.run(main(urls, bot_api, admin_id))
